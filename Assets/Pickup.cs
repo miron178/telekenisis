437a4acecd5a57e250f23bdb8d4ff02462940b57
Miron Bury <miron.bury@gmail.com>
2021-11-13 17:09:31 +0000

    partially solved object bounce and drop
    drop needs to be more forgiving

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -7,20 +7,54 @@ public class Pickup : MonoBehaviour
     [SerializeField]
     private Transform destination;
 
-    private void OnMouseDown()
+    private bool m_held = false;
+    private void Grab()
     {
-        GetComponent<Rigidbody>().velocity = Vector3.zero;
-        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-        GetComponent<BoxCollider>().enabled  = false;
+        //disable collision
+        GetComponent<BoxCollider>().enabled = true; //activating partialy solves problem (loop code above to solve problem permanently)
+
+        //stop gravity
         GetComponent<Rigidbody>().useGravity = false;
+
         //this.transform.position = destination.position;
         this.transform.parent = destination.transform;
+
+        m_held = true;
     }
 
-    private void OnMouseUp()
+    private void Release()
     {
         this.transform.parent = null;
-        GetComponent<BoxCollider>().enabled  = true;
+        GetComponent<BoxCollider>().enabled = true;
         GetComponent<Rigidbody>().useGravity = true;
+        m_held = false;
     }
+
+    private void OnMouseDrag()
+    {
+        if (m_held)
+        {
+            //stop object from floating or turning
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+
+    }
+
+    private void OnMouseDown() //may want to use mouse up and down as axis
+    {
+        Grab();
+    }
+
+    private void OnMouseExit()
+    {
+        Release();
+    }
+    private void OnMouseUp()
+    {
+        Release();
+    }
+
+    //get player dir
+    //use mousewheel to move object along player dir
 }
