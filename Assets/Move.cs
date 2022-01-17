b96d58e8c396e402c9ae1c9bb9eafe806918732f
Miron Bury <miron.bury@gmail.com>
2022-01-17 20:23:55 +0000

    can stay on platform
    platform cannot have scale

---
+++ Assets/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.0f;
+
+    [SerializeField]
+    private Vector3 direction = Vector3.forward;
+
+    [SerializeField]
+    private float swapDelay = 3.0f;
+
+    private float swapTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        swapTime = Time.time + swapDelay;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (swapTime <= Time.time)
+        {
+            swapTime = Time.time + swapDelay;
+            direction = direction * -1.0f;
+        }
+
+        transform.position += direction * speed * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup") || other.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.SetParent(null);
+    }
+}
