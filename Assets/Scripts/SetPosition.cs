583db93fefab869211918f43c9528223a3e65514
Miron Bury <miron.bury@gmail.com>
2022-05-05 01:37:32 +0100

    Add script to set pos of objects with buttons

---
+++ Assets/Scripts/SetPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosition : MonoBehaviour
+{
+    //[SerializeField]
+    //private Transform m_destination;
+
+    public void MoveTo(Transform destination)
+    {
+        this.gameObject.transform.position = destination.position;
+    }
+}
