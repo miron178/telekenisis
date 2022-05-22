e6297edce10ead8416b47cd772a00fd01e4a647b
Miron Bury <miron.bury@gmail.com>
2022-05-22 18:44:24 +0100

    re make button script so that the new script isn't triggered by objects or grabs

---
+++ Assets/Trap.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent m_onPress;
+
+    [SerializeField]
+    UnityEvent m_onRelease;
+
+    [SerializeField]
+    bool m_activeOnStart = false;
+
+    public void Press() => m_onPress?.Invoke();
+
+    public void Release() => m_onRelease?.Invoke();
+
+    private void Start()
+    {
+        if (m_activeOnStart)
+        {
+            Press();
+        }
+        else
+        {
+            Release();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Press();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Release();
+        }
+    }
+}
