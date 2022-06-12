d216e015f034b71fec172614abf0fb3afbb32815
Miron Bury <miron.bury@gmail.com>
2022-06-12 18:50:38 +0100

    allow return to hub. fixed level select Emission

---
+++ Assets/My Project Files/Scripts/TpToHub.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpToHub : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_destination;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Esc") > 0)
+        {
+            this.GetComponent<CharacterController>().enabled = false;
+            this.gameObject.transform.position = m_destination.position;
+            this.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+}
